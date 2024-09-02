fe74e578aab1c6cbac7bb30b036bab682981cc6d
MichalPolak01 <michalpl618@gmail.com>
2024-09-02 16:05:03 +0200

    Initial commit

---
+++ src/waitlists/api.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from django.shortcuts import get_object_or_404
+from ninja import Router
+from ninja_jwt.authentication import JWTAuth
+
+from .models import WaitlistEntry
+from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema
+
+router = Router()
+
+
+@router.get("", response=List[WaitlistEntryListSchema], auth=JWTAuth())
+def list_waitlist_entries(request):
+    qs = WaitlistEntry.objects.all()
+    return qs
+
+
+@router.get("{entry_id}", response=WaitlistEntryDetailSchema, auth=JWTAuth())
+def get_waitlist_entry(request, entry_id:int):
+    obj =get_object_or_404(WaitlistEntry, id=entry_id)
+    return obj
---
+++ src/waitlists/schemas.py
@@ -0,0 +1,20 @@
+from ninja import Schema
+from datetime import datetime
+from pydantic import EmailStr
+
+
+class WaitlistEntryCreateSchema(Schema):
+    email: EmailStr
+
+
+class WaitlistEntryListSchema(Schema):
+    id: int
+    email: EmailStr
+    
+
+
+class WaitlistEntryDetailSchema(Schema):
+    id: int
+    email: EmailStr
+    updated: datetime
+    timestamp: datetime
